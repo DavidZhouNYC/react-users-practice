552a2b5356055d8968731f66ddc60c8b1c5e7a9f
David Zhou <11278771+DavidZhouNYC@users.noreply.github.com>
2022-08-07 14:47:24 -0400

    Fix typo

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -28,7 +28,7 @@ const AddUser = (props) => {
         }
 
         if (+enteredUserAge < 1) {
-            setError({0
+            setError({
                 title: "Invalid age",
                 message: "Please enter a valid age (> 0).",
             });
